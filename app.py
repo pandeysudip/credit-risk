e203ce0740da20595857c736d68e5a55aeef1bb2
pandeysudip <sudip@siu.edu>
2022-01-06 18:30:52 -0800

    flask

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 from flask import Flask, render_template, redirect, jsonify, request
 import utilis
 from flask_sqlalchemy import SQLAlchemy
-#from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.ext.automap import automap_base
 import os
 # Create an instance of Flask
 app = Flask(__name__)
 
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '')
+# 'postgresql://localhost:5432/credit-risk-eval'
+
 
 # Remove tracking modifications
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -15,34 +17,32 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 # getting the tables from database-1st approach
-test = db.Table('train', db.metadata, autoload=True, autoload_with=db.engine)
-train = db.Table('test', db.metadata, autoload=True, autoload_with=db.engine)
-test_dumy = db.Table('train_dumy', db.metadata,
-                     autoload=True, autoload_with=db.engine)
-train_dumy = db.Table('test_dumy', db.metadata,
-                      autoload=True, autoload_with=db.engine)
+# test = db.Table('train', db.metadata, autoload=True, autoload_with=db.engine)
+# train = db.Table('test', db.metadata, autoload=True, autoload_with=db.engine)
+# test_dumy = db.Table('train_dumy', db.metadata,autoload = True, autoload_with = db.engine)
+# train_dumy = db.Table('test_dumy', db.metadata,autoload=True, autoload_with=db.engine)
 # query be like: db.session.query(train).all()
 
 
 # getting the tables from database-2nd approach
 # reflect an existing database into a new model
-#Base = automap_base()
-#Base.prepare(db.engine, reflect=True)
+Base = automap_base()
+Base.prepare(db.engine, reflect=True)
 
 # Save references to each table
 #train = Base.classes.train
 #test = Base.classes.test
-#train_dumy = Base.classes.train_dumy
-#test_dumy = Base.classes.test_dumy
+train_dumy = Base.classes.train_dumy
+test_dumy = Base.classes.test_dumy
 
 
-@app.route('/')
+@ app.route('/')
 def home():
     # Return the template
     return render_template('index.html')
 
 
-@app.route('/send', methods=["GET", "POST"])
+@ app.route('/send', methods=["GET", "POST"])
 def predic():
 
     if request.method == "POST":
@@ -50,7 +50,7 @@ def predic():
         interest_rate = request.form.get("int_rate")
         installement = request.form.get("installment")
         anual_income = request.form.get('anual_inc')
-        #home_owenership = request.form.get("home_ownership")
+        # home_owenership = request.form.get("home_ownership")
 
         variables = [loan_amount, interest_rate,
                      installement,  anual_income]
